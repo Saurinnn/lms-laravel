53d8941abbb5a9f5f0fc1bedd6089d9549127a49
Saurinnn <44335507+Saurinnn@users.noreply.github.com>
2023-03-29 15:52:36 +0900

    sqlite

---
+++ docker-laravel/src/tests/Feature/HelloTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Models\User;
+use DatabaseMigrations;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+//use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class HelloTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    /*public function test_example()
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }*/
+
+    public function testHello(){
+        $this->assertTrue(true);
+
+        $response = $this->get('/');
+        $response->assertStatus(200);
+
+        $response = $this->get('/hello');
+        $response->assertStatus(302);
+
+        $user = factory(User::class)->create();
+        $response = $this->actingAs($user)->get('/hello');
+        $response->assertStatus(200);
+
+        $response = $this->get('/no_route');
+        $response->assertStatus(404);
+    }
+}
