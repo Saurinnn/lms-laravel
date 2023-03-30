b7907a2bb50389ed3f215cc8277f15365b05ad3c
Saurinnn <44335507+Saurinnn@users.noreply.github.com>
2023-03-30 15:54:23 +0900

    テスト実行

--- docker-laravel/src/app/Models/User.php
+++ docker-laravel/src/app/Models/User.php
@@ -17,12 +17,12 @@ class User extends Authenticatable
      *
      * @var array<int, string>
      */
-    protected $fillable = [
+     protected $fillable = [
         'name',
         'email',
         'password',
     ];
-
+    public $timestamps = false;
     /**
      * The attributes that should be hidden for serialization.
      *
--- docker-laravel/src/tests/Feature/HelloTest.php
+++ docker-laravel/src/tests/Feature/HelloTest.php
@@ -6,13 +6,15 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 use App\Models\User;
-use DatabaseMigrations;
+use App\Models\Person;
+//use DatabaseMigrations;
 use Illuminate\Foundation\Testing\WithoutMiddleware;
 //use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Illuminate\Foundation\Testing\DatabaseTransactions;
+//use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class HelloTest extends TestCase
 {
+    use RefreshDatabase;
     /**
      * A basic feature test example.
      *
@@ -26,7 +28,7 @@ class HelloTest extends TestCase
     }*/
 
     public function testHello(){
-        $this->assertTrue(true);
+        /*$this->assertTrue(true);
 
         $response = $this->get('/');
         $response->assertStatus(200);
@@ -34,11 +36,38 @@ class HelloTest extends TestCase
         $response = $this->get('/hello');
         $response->assertStatus(302);
 
-        $user = factory(User::class)->create();
+        $user = User::factory()->create();
         $response = $this->actingAs($user)->get('/hello');
         $response->assertStatus(200);
 
         $response = $this->get('/no_route');
-        $response->assertStatus(404);
+        $response->assertStatus(404);*/
+        //ダミーデータ
+        User::factory()->create([
+            'name' => 'AAA',
+            'email' => 'BBB@CCC.COM',
+            'password' => 'ABCABC',
+
+        ]);
+        User::factory(10)->create();
+
+        $this->assertDatabaseHas('users',[
+            'name' => 'AAA',
+            'email' => 'BBB@CCC.COM',
+            'password' => 'ABCABC',
+        ]);
+        //ダミーデータ
+        Person::factory()->create([
+            'name' => 'XXX',
+            'mail' => 'YYY@ZZZ.COM',
+            'age' => 123,
+        ]);
+        Person::factory(10)->create();
+
+        $this->assertDatabaseHas('people',[
+            'name' => 'XXX',
+            'mail' => 'YYY@ZZZ.COM',
+            'age' => 123,
+        ]);
     }
 }
